f6939c9c01098b1211f2216aaaead86e4e0ed201
Arjun Gupta <arjuntheprogrammer@gmail.com>
2026-01-14 22:51:44 +0530

    setOrigin

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -790,6 +790,7 @@ function createPicker(config) {
     )
     .setOAuthToken(config.accessToken)
     .setDeveloperKey(config.apiKey)
+    .setOrigin(window.location.origin)
     .setCallback(pickerCallback)
     .enableFeature(google.picker.Feature.SUPPORT_DRIVES)
     .build();
